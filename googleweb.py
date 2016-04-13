c2f8b21c7a15a6f9a7a0af8fc17df368c7fa08a9
cgboy <cgboy@users.noreply.github.com>
2016-04-13 13:10:48 +0800

    Update googleweb.py

--- googleweb.py
+++ googleweb.py
@@ -5,32 +5,34 @@ try:
 except ImportError:
     from http.server import SimpleHTTPRequestHandler as Handler
     from http.server import HTTPServer as Server
+from  http import HTTPStatus
 import logging
 import logging.handlers
 import os
+
+loger = logging.getLogger('googleweb')
+loger.setLevel(logging.INFO)
+fh = logging.handlers.RotatingFileHandler(os.path.join(os.getcwd(), 'connect.log'), maxBytes=5242880, backupCount=5)
+fh.suffix = '%Y-%m-%d'
+formatter = logging.Formatter('%(asctime)s  %(message)s', '%a %d %b %Y %H:%M:%S')
+fh.setFormatter(formatter)
+loger.addHandler(fh)
+
 class GoogleHandler(Handler):
+
     def do_GET(self):
         if self.path=='/generate_204':
-            self.send_response(204,'')
-            self.end_headers()	
-            self.writeLog(str(self.client_address[0]) + '  ' + str(self.headers['User-Agent']))
-    def writeLog(self,msg):
+            self.send_response(HTTPStatus.NO_CONTENT)
+            self.end_headers()
+            info=str(self.client_address[0]) + '  ' + str(self.headers['User-Agent'])
+            loger.info(info)
 
-        loger = logging.getLogger('googleweb')
-        loger.setLevel(logging.INFO)
-        fh = logging.handlers.TimedRotatingFileHandler(os.path.join(os.getcwd(),'log.log'),'D',1,30)
-        fh.suffix='%Y-%m-%d'
-        formatter = logging.Formatter('%(asctime)s  %(message)s','%a %d %b %Y %H:%M:%S')
 
-        fh.setFormatter(formatter)
-        loger.addHandler(fh)
-        loger.info(msg)
 httpd=Server(('',80),GoogleHandler)
 try:
     httpd.serve_forever()
 except KeyboardInterrupt:
     pass
 httpd.server_close()
- 
 
 
