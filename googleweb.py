607135121f80772e805bb4e28894be9fbf645650
cgboy <cgboy@users.noreply.github.com>
2016-04-13 16:36:02 +0800

    Added files via upload

--- googleweb.py
+++ googleweb.py
@@ -1,18 +1,18 @@
-#! /usr/local/bin/python3
+#!/usr/bin/python3
 try:
     from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
     from SocketServer import TCPServer as Server
 except ImportError:
     from http.server import SimpleHTTPRequestHandler as Handler
     from http.server import HTTPServer as Server
-from  http import HTTPStatus
+
 import logging
 import logging.handlers
 import os
-
+import sys
 loger = logging.getLogger('googleweb')
 loger.setLevel(logging.INFO)
-fh = logging.handlers.RotatingFileHandler(os.path.join(os.getcwd(), 'connect.log'), maxBytes=5242880, backupCount=5)
+fh = logging.handlers.RotatingFileHandler(os.path.join(sys.path[0], 'connect.log'),maxBytes=5242880, backupCount=5)
 fh.suffix = '%Y-%m-%d'
 formatter = logging.Formatter('%(asctime)s  %(message)s', '%a %d %b %Y %H:%M:%S')
 fh.setFormatter(formatter)
@@ -22,7 +22,7 @@ class GoogleHandler(Handler):
 
     def do_GET(self):
         if self.path=='/generate_204':
-            self.send_response(HTTPStatus.NO_CONTENT)
+            self.send_response(204)
             self.end_headers()
             info=str(self.client_address[0]) + '  ' + str(self.headers['User-Agent'])
             loger.info(info)
